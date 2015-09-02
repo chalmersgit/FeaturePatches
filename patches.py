29198278fe50d93d00aa155c58ff4e6f81a2df2b
Andrew Chalmers <andrewchalmers26@gmail.com>
2015-09-02 16:51:35 +1200

    First commit

---
+++ patches.py
@@ -0,0 +1,44 @@
+import numpy as np
+from skimage.util.shape import view_as_windows
+
+"""
+References:
+
+"Texture Classification from Random Features"
+2012, L. Liu and W. Fieguth)
+http://vip.uwaterloo.ca/files/publications/Paper1JournalLiFieguth_final.pdf
+"""
+
+def getPatches(arr, patchSize, step=1):
+	return view_as_windows(arr, patchSize, step)
+
+def transToFeatVecs(patches):
+	return patches.reshape(getNumPatches(patches),getNumPatchElements(patches))
+
+def getNumPatches(patches):
+	return patches.shape[0]*patches.shape[1]
+
+def getNumPatchElements(patches):
+	return patches.shape[2]*patches.shape[3]
+
+if __name__ == "__main__":
+	print 'Patches'
+	width 		= 200
+	height 		= 200
+
+	N 			= int(width*height)		# total elements
+	patchSize 	= int(np.sqrt(width))	# square patch resolution using width
+
+	arr = np.arange(N).reshape(height,width)
+
+	print 'Width x Height:\t', width,'x',height
+	print 'Total elements:\t',N
+	print 'Patch size:\t', patchSize
+	print
+
+	patches = getPatches(arr, (patchSize,patchSize), patchSize)
+	features = transToFeatVecs(patches)
+
+	print patches.shape
+	print features.shape
+
---
+++ randomProjection.py
@@ -0,0 +1,70 @@
+import numpy as np
+import math
+from sklearn import random_projection
+
+import patches as pa 
+
+"""
+Reduces the dimensionality of feature vectors using random projection.
+Feature vectors are obtained from patches of a matches using the patches module
+
+References:
+
+Johnson-Lindenstrauss Random Projection
+2009, S. Kakade and G. Shakhnarovich
+http://ttic.uchicago.edu/~gregory/courses/LargeScaleLearning/lectures/jl.pdf
+
+"Database-friendly random projections: Johnson-Lindenstrauss with binary coins" 
+2002, D. Achlioptas
+https://users.soe.ucsc.edu/~optas/papers/jl.pdf
+
+"Texture Classification from Random Features"
+2012, L. Liu and W. Fieguth)
+http://vip.uwaterloo.ca/files/publications/Paper1JournalLiFieguth_final.pdf
+
+Stackoverflow:
+http://stackoverflow.com/questions/7474508/random-projection-algorithm-pseudo-code
+"""
+
+def randomProjection(X, verbose = False):
+	"""
+	X should be of shape (N,M)
+	Where N is the number of features 
+	and M is the number of dimensions 
+	"""
+	numFeats 	= X.shape[0]
+	numDims 	= X.shape[1]
+
+	k			= (20 * np.log(numFeats)) / (math.e*math.e)
+	reducedDims = int(k)
+	norm 		= 1.0 / (np.sqrt(k))
+
+	randMat		= (np.ones((numDims,reducedDims))*np.clip(1, -1, np.random.normal(0, 1, (numDims,reducedDims)))).astype(int)
+	X_reduced	= norm*np.dot(X,randMat)
+	
+	if verbose:
+		print "Original dimensions:\t", X.shape
+		print "Reduced dimensions:\t", X_reduced.shape
+
+	return X_reduced
+
+if __name__ == "__main__":
+	print 'Random Projection'
+	width 		= 200
+	height 		= 200
+
+	N 			= int(width*height)		# total elements
+	patchSize 	= int(np.sqrt(width))	# square patch resolution using width
+
+	arr = np.arange(N).reshape(height,width)
+
+	print 'Width x Height:\t', width,'x',height
+	print 'Total elements:\t',N
+	print 'Patch size:\t', patchSize
+	print
+
+	patches = pa.getPatches(arr, (patchSize,patchSize), patchSize)
+	features = pa.transToFeatVecs(patches)
+
+	featuresProj = randomProjection(features, True)
+
